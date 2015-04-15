17d1cca253d079aef17313c57e69bf720681ee17
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2015-04-15 16:00:53 +0200

    Create and delete lambda functions

--- src/aws_lambda.erl
+++ src/aws_lambda.erl
@@ -1,8 +1,8 @@
 -module(aws_lambda).
 
--export([get_function/3, invoke/4]).
+-export([get_function/3, invoke/4, create_function/7, delete_function/1]).
 %% Application api
--ignore_xref([get_function/3, invoke/4]).
+-ignore_xref([get_function/3, invoke/4, create_function/7, delete_function/1]).
 
 
 -define(API_VERSION, "2015-03-31").
@@ -17,3 +17,38 @@ invoke(Name, Payload, Headers0, Opts) ->
     Path = [?API_VERSION, "functions", Name, "invocations"],
     Headers = [{"Content-Type", "application/json"}|Headers0],
     aws_http:post(Path, Headers, Payload, ?SERVICE, Opts).
+
+create_function(Name, ZipBin, Handler, Role, Runtime, Headers0, Opts) ->
+    Payload0 = #{<<"Code">> => #{<<"ZipFile">> => base64:encode(ZipBin)},
+                 <<"FunctionName">> => bin(Name),
+                 <<"Handler">> => bin(Handler),
+                 <<"Runtime">> => bin(Runtime),
+                 <<"Role">> => bin(Role)},
+    OptArgs = [{<<"Timeout">>, lambda_timeout},
+               {<<"MemorySize">>, lambda_memory_size},
+               {<<"Description">>, lambda_description}],
+    Payload = playload_opt_add(Payload0, OptArgs, Opts),
+    Path = [?API_VERSION, "functions"],
+    Headers = [{"Content-Type", "application/json"}|Headers0],
+    aws_http:post(Path, Headers, Payload, ?SERVICE, Opts).
+
+delete_function(Name) ->
+    Path = [?API_VERSION, "functions", Name],
+    aws_http:delete(Path, [], ?SERVICE, []).
+
+%% ---------------------------------------------------------------------------
+%% Internal
+
+bin(Bin) when is_binary(Bin) -> Bin;
+bin(IoList) when is_list(IoList) -> iolist_to_binary(IoList).
+
+playload_opt_add(Payload, Vals, Opts) ->
+    lists:foldl(
+      fun({PayloadName, OptName}, PayloadAcc) ->
+              case proplists:get_value(OptName, Opts) of
+                  undefined -> PayloadAcc;
+                  Val -> maps:put(PayloadName, Val, PayloadAcc)
+              end
+      end,
+      Payload,
+      Vals).
